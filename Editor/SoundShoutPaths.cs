ce1049f58938dc70242c69d7017b124ed05fbf45
Niklas Mikkelsen <mikkelsenniklas@gmail.com>
2022-03-28 02:19:17 +0200

    Update SoundShoutPaths.cs
    Renamed ROOT_PATH -> ROOT_PACKAGE_PATH

--- Editor/SoundShoutPaths.cs
+++ Editor/SoundShoutPaths.cs
@@ -2,15 +2,15 @@
 {
     internal static class SoundShoutPaths
     {
-        internal const string ROOT_PATH = "Packages/se.somethingwemade.soundshout/";
-        internal const string SETTINGS_ASSET_PATH = ROOT_PATH + "Editor/Settings/Sound Shout Settings.asset";
+        private const string ROOT_PACKAGE_PATH = "Packages/se.somethingwemade.soundshout/";
+        internal const string SETTINGS_ASSET_PATH = ROOT_PACKAGE_PATH + "Editor/Settings/Sound Shout Settings.asset";
         
         
         internal const string CLIENT_SECRET_PATH = TOOL_PATH + "/client_secret.json";
-        internal const string TOOL_PATH = ROOT_PATH + "/Editor/EditorWindow";
+        internal const string TOOL_PATH = ROOT_PACKAGE_PATH + "/Editor/EditorWindow";
 
 
-        internal const string TOOL_LOGO_PATH = SoundShoutPaths.TOOL_PATH + "/SS_Tool_Logo.png";
-        internal const string AUDIO_REFERENCE_ICON_PATH = SoundShoutPaths.TOOL_PATH + "/SS_Asset_Logo.png";
+        internal const string TOOL_LOGO_PATH = TOOL_PATH + "/SS_Tool_Logo.png";
+        internal const string AUDIO_REFERENCE_ICON_PATH = TOOL_PATH + "/SS_Asset_Logo.png";
     }
 }
